6ff7cddbebe4dd25d5acf59e6bfdc3287bdf9144
Tony Childs <tony.childs@gmail.com>
2020-05-02 00:27:10 -0500

    Allow moving to previous/next page of results

--- week-5/recitation/starwars_planets/src/Planets.jsx
+++ week-5/recitation/starwars_planets/src/Planets.jsx
@@ -4,19 +4,70 @@ import Planet from "./Planet";
 import "./styles.css";
 
 export default function Planets() {
+  /**
+   * Array of planet objects
+   */
   const [planets, setPlanets] = React.useState([]);
-  React.useEffect(() => {
-    axios("https://swapi.dev/api/planets/").then((resp) => {
-      // breakpoint;
-      setPlanets(resp.data.results);
+
+  /**
+   * The URL to the next page or results
+   */
+  const [next, setNext] = React.useState(null);
+
+  /**
+   * The URL to the previous page or results
+   */
+  const [prev, setPrev] = React.useState(null);
+
+  /**
+   * Retrieve a page of results and update state variables
+   *
+   * @param {string} url - The URL of the results page to retrieve
+   */
+  const updateResults = (url) => {
+    axios(url).then((resp) => {
+      const { previous, next, results } = resp.data;
+      setPrev(previous);
+      setNext(next);
+      setPlanets(results);
     });
-  }, []);
+  };
+
+  /**
+   * Get initial page of results
+   */
+  React.useEffect(
+    updateResults.bind(null, "https://swapi.dev/api/planets/"),
+    []
+  );
+
+  /**
+   * Get the previous page of results
+   */
+  const prevPage = () => {
+    updateResults(prev);
+  };
+
+  /**
+   * Get the next page of results
+   */
+  const nextPage = () => {
+    updateResults(next);
+  };
 
   return (
     <React.Fragment>
       {planets.map((planet) => (
         <Planet key={planet.url} planet={planet} />
       ))}
+      <div>
+        <button disabled={!prev} onClick={prevPage}>
+          &lt; Prev
+        </button>
+        <button disabled={!next} onClick={nextPage}>
+          Next &gt;
+        </button>
+      </div>
     </React.Fragment>
   );
 }
